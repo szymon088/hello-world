5193db1eab0d4499419a5d5396f68710e1d6af4b
szymon088 <szymonzak088@gmail.com>
2016-09-01 19:49:38 +0200

    First files

---
+++ script.js
@@ -0,0 +1,7 @@
+   var request = new XMLHttpRequest();
+   request.open("GET", "dane.json", false);
+   request.send(null)
+   var my_JSON_object = JSON.parse(request.responseText);
+   document.getElementById('par').innerHTML = my_JSON_object.name;
+
+

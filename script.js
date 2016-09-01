0dba43f76b7ecd73a3ad16bc0ea4c6383bb8e2d3
szymon088 <szymonzak088@gmail.com>
2016-09-01 19:51:55 +0200

    Adding console.log

--- script.js
+++ script.js
@@ -3,5 +3,6 @@
    request.send(null)
    var my_JSON_object = JSON.parse(request.responseText);
    document.getElementById('par').innerHTML = my_JSON_object.name;
+   console.log("New commit")
 
 
